e6a8bb6154da28005237460d7524989f1441310a
Patrick Posner <hello@patrickposner.dev>
2025-04-27 23:13:55 +0900

    bring back all commands

--- install/install.sh
+++ install/install.sh
@@ -60,14 +60,14 @@ php /app/wp-cli.phar \
 
 echo "Installing Simply Static and Simply Static Pro"
 php /app/wp-cli.phar --allow-root --path=/app plugin install simply-static --activate;
-# php /app/wp-cli.phar --allow-root --path=/app plugin install https://api.static.studio/storage/v1/object/public/plugins/simply-static-pro.zip --activate;
+php /app/wp-cli.phar --allow-root --path=/app plugin install https://api.static.studio/storage/v1/object/public/plugins/simply-static-pro.zip --activate;
 
 echo "Install and activate Ollie theme."
 php /app/wp-cli.phar --allow-root --path=/app theme install ollie --activate;
 
 echo "Installing MU plugin".
+php /app/wp-cli.phar --allow-root --path=/app plugin install https://api.static.studio/storage/v1/object/public/plugins/simply-static-studio-helper.zip;
+mv /app/wp-content/plugins/simply-static-studio-helper/ /app/wp-content/mu-plugins/
 curl -L https://api.static.studio/storage/v1/object/public/plugins/load.php -o /app/wp-content/mu-plugins/load.php;
-curl -L https://api.static.studio/storage/v1/object/public/plugins/simply-static-studio-helper.zip -o /app/wp-content/mu-plugins/simply-static-studio-helper.zip;
-unzip /app/wp-content/mu-plugins/simply-static-studio-helper.zip
 
 echo "Installation complete"
