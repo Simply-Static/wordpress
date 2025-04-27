cd20e587493b6605f946837397ec7fe24dd35e0c
Patrick Posner <hello@patrickposner.dev>
2025-04-27 22:46:17 +0900

    enhanced install.sh

--- install/install.sh
+++ install/install.sh
@@ -11,6 +11,9 @@ echo "Creating required directories..."
 mkdir -p /app/wp-content/plugins
 echo "" > /app/wp-content/plugins/.keep
 
+mkdir -p /app/wp-content/mu-plugins
+echo "" > /app/wp-content/mu-plugins/.keep
+
 mkdir -p /app/wp-content/upgrade
 echo "" > /app/wp-content/upgrade/.keep
 
@@ -33,6 +36,19 @@ php /app/wp-cli.phar --allow-root --path=/app \
   media import "https://i0.wp.com/learn.wordpress.org/files/2023/08/WordPress-logotype-simplified.png" --porcelain | \
   php /app/wp-cli.phar --allow-root --path=/app option update site_icon || true
 
+echo "Set up constants..."
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_HAS_MIGRATION "$SSS_HAS_MIGRATION";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_SECRET_KEY "$SSS_SECRET_KEY";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_ACCESS_KEY "$SSS_ACCESS_KEY";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_PULL_ZONE "$SSS_PULL_ZONE";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_STORAGE_ZONE "$SSS_STORAGE_ZONE";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_STORAGE_HOST "$SSS_STORAGE_HOST";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_SITE_ID "$SSS_SITE_ID";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_EMAIL "$SSS_EMAIL";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_LICENSE "$SSS_LICENSE";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_BASIC_AUTH_USER "$SSS_BASIC_AUTH_USER";
+php /app/wp-cli.phar --allow-root --path=/app config set SSS_BASIC_AUTH_PASSWORD "$SSS_BASIC_AUTH_PASSWORD";
+
 echo "Installing theme and plugins..."
 
 php /app/wp-cli.phar \
@@ -41,7 +57,13 @@ php /app/wp-cli.phar \
   wasmer-aio-install install \
   --locale="$WP_LOCALE" \
   --theme=/app/install/ollie.zip || true
-  --plugin=/app/install/simply-static.zip || true
-  --plugin=/app/install/simply-static-pro.zip || true
+
+echo "Installing Simply Static and Simply Static Pro"
+php /app/wp-cli.phar --allow-root --path=/app plugin install simply-static --activate;
+php /app/wp-cli.phar --allow-root --path=/app plugin install /app/install/simply-static-pro.zip --activate;
+
+echo "Installing MU plugin".
+php /app/wp-cli.phar --allow-root --path=/app plugin install /app/install/simply-static-studio-helper.zip;
+
 
 echo "Installation complete"
